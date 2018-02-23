ddb0db4f074434bdeb8e72d4062f9c5ccc1bb4c4
nselof <selof86@gmail.com>
2018-02-23 15:32:10 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,116 @@
+# async-flow
+A promise-based orchestration tool for asynchronous javascript.
+
+# Setup
+npm install async-routine
+
+*Node.js*
+```
+const AsyncRoutine = require('async-flow-routine');
+```
+
+*Angular.js*
+```
+<script src="libs/async-routine/async-routine.js"></script>
+
+angular
+  .run(["$q", function ($q) {
+    AsyncRoutine.Promise = $q;    
+  }]);
+```
+
+# Usage
+```
+new AsyncRoutine()
+  .first('connection', prGetConnection)
+  
+  .then('users', r => r.connection.prFetch('users'))
+  .and('userRoles', r => new Promise( (resolve, reject) {
+    r.connection.cbFetch('userRoles', function (err, results) {
+      if (err) return reject(err);
+      
+      reutrn resolve(results);
+    })
+  })
+  
+  .then(r => r.connection.close())
+  
+  // execute routine
+  .exec()
+  .then(r => {
+    delete r.connection;
+    client.json(r);
+  })
+  .catch(err => {
+    // receives any reject in routine
+    client.error(err);
+  });
+```
+
+#  Routine
+Build a routine by stringing together actions with expressive methods - .first, .last, .then, .and, .with
+
+*Syncronous*
+Use the methods .then, .first, .last to run these actions one after the next, waiting for the previous actions to complete before running.
+
+*Asyncronous*
+Use the methods .and, .with to run these actions simultaneously with the previous actions.
+
+```
+new AsyncRoutine()
+
+  .first(action1)
+  .and(action2)
+  .and(action3) // these three tasks will run Asyncronously (at the same time)
+
+  .then(action4) // use .then() to wait for all previous actions to complete
+  .and(action5)
+
+  .last(action6)
+```
+
+*Action*
+Action Key (optional): This string is used as the property key appended to the *responses* object with the results of the Action Function given.
+Action Function: A function that receives the *responses* object and may or may not return a value. If it returns a promise, the results of that promise will be stored in the responses object when they come in, or else the non-promise return variable will be immediately stored the responses object.
+
+```
+  .then('actionKey', actionFn)
+```
+
+*Responses*
+This object accumulates the responses of each action that is given a key. Each response will be available on the next .then() action.
+
+```
+new AsyncRoutine()
+  .first('users', prGetUsers)
+  .and( responses => {
+    console.log(responses.users); // undefined
+  })
+
+  .then( responses => {
+    console.log(responses.users); // [user1, user2, user3...]
+  })
+
+```
+
+*Execute*
+Be sure to .exec() the routine to run it all. .exec() will return a promise.
+
+
+*Pro-Tip*
+Build the routine dynamically.
+
+```
+let routine = new AsyncRoutine()
+  .first('connection', prGetConnection)
+  .wait(); // use .wait().and() if .then() can't be done
+
+for (let file of uploads) {
+  routine.and(() => prUploadFile(file));
+}
+
+routine
+  .exec()
+  .then(...)
+  .catch(...);
+```
---
+++ async-routine.js
@@ -0,0 +1,149 @@
+'use strict';
+
+function AsyncRoutine() {
+  let self = this;
+
+  /////////////////////////////////////////////// UTILITIES ////
+
+  // default promise, override by setting AsyncRoutine.Promise = $q
+  self.Promise = function(fn) {
+    return new Promise(fn);
+  }
+
+  self.PromiseResolve = function(value) {
+    return self.Promise( function (resolve, reject) {
+      return resolve(value);
+    });
+  }
+
+  /////////////////////////////////////////////// ROUTINE BUILDING METHODS ////
+
+  self.routine = [];
+  self.currentActions = null;
+
+  self.then = function(keyOrFn, possibleFn) {
+    self.wait();
+    return self.and(keyOrFn, possibleFn);
+  }
+
+  self.wait = function(){
+    // make a new group of actions
+    self.currentActions = [];
+    self.routine.push(self.currentActions);
+  }
+
+  self.action = function(keyOrFn, possibleFn) {
+    // add this action to current group
+    if (typeof possibleFn === 'function') {
+      let step = {};
+      step[keyOrFn] = possibleFn;
+      self.currentActions.push(step);
+    } else {
+      self.currentActions.push(keyOrFn);
+    }
+    return self;
+  }
+
+  // setup aliases
+  self.first = self.after = self.last = self.then;
+  self.and = self.with = self.action;
+  
+  ///////////////////////////////////////////// ACTION PROCESSING ////
+
+  self.exec = function() { // returns a promise
+    self.responses = {};
+    self.completion = [];
+    self.isComplete = false;
+    self.promise = self.Promise( function (resolve, reject) {
+      // save resolve and reject actions for other methods
+      self.resolve = function () {
+        self.isComplete = true;
+        return resolve(self.responses);
+      };
+      self.reject = function (err) {
+        self.isComplete = true;
+        return reject(err);
+      };
+      // start the steps
+      self.doStep(0);
+    });
+    return self.promise;
+  }
+
+  self.doStep = function (index) {
+    if (self.isComplete) {
+      return;
+    }
+
+    // check if previous step (group of actions) is completed
+    if (index != 0) {
+      let curCompletion = self.completion[index - 1];
+      for (let i = 0; i < curCompletion.length; i++) {
+        if (!curCompletion[i]) {
+          return;
+        }
+      }
+    }
+
+    // if this step doesn't exist, we're done
+    if (index == self.routine.length) {
+      return self.resolve();
+    }
+
+    // setup next step callback
+    let nextStep = function () {
+      self.doStep(index + 1);
+    };
+
+    // get actions for current step
+    let actions = self.routine[index];
+
+    // execute actions in current step
+    self.doActions(actions, nextStep);
+  };
+
+  self.doActions = function (actions, nextStep) {
+    let stepCompletion = [];
+    self.completion.push(stepCompletion);
+
+    actions.forEach(function (action) {
+      // get and check the function
+      let fn = typeof action === 'object' ? action[Object.keys(action)[0]] : action;
+
+      if (typeof fn !== 'function') {
+        self.reject('Action is not a Function');
+      }
+
+      // run the function
+      let possiblePromise = fn(self.responses);
+
+      // check if no response
+      if (typeof possiblePromise === 'undefined') {
+        stepCompletion.push(true);
+        return nextStep();
+      } else {
+        stepCompletion.push(false);
+        // convert response into a promise (if it isn't already)
+        let definitelyPromise = typeof possiblePromise.then === "function" ?
+          possiblePromise :  
+          PromiseResolve(possiblePromise);
+
+        definitelyPromise
+          .then( function (response) {
+            // mark as complete
+            stepCompletion[actions.indexOf(action)] = true;
+            // should we put this in responses?
+            if (typeof action === 'object') {
+              self.responses[Object.keys(action)] = response;
+            }
+            return nextStep();
+          })
+          .catch(self.reject);
+      }
+    });
+  };
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = exports = AsyncRoutine;
+}
